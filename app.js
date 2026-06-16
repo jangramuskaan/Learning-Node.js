4a1d64852b734385be0f804bb238d5f87516b2ce
Muskaan Jangra <jangra.muskaan.mj@gmail.com>
2026-06-16 13:07:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,4 +10,32 @@
 // 2.The Event Module and EventEmitter Class 
 
 const EventEmitter = require('events');
-const EventEMitter = new EventEmitter(); 
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('tutorial', (num1,num2) => {
+    console.log("num1 + num2 = ", num1 + num2);
+});
+
+eventEmitter.emit('tutorial', 1,2);
+
+class Person extends EventEmitter {
+    constructor(name){
+        super();
+        this._name = name;
+    }
+    get name(){
+        return this._name;
+    }
+}
+
+let muskaan = new Person('Muskaan');
+let christina = new Person('Christina');
+christina.on('name', () =>{
+    console.log("My name is ", christina.name)
+});
+muskaan.on('name', () =>{
+    console.log("My name is ", muskaan.name)
+});
+
+muskaan.emit('name');
+christina.emit('name');
