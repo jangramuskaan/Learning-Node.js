ebff76fed9478b663ee09caf10aabf7577559647
Muskaan Jangra <jangra.muskaan.mj@gmail.com>
2026-06-16 12:02:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// 1. Working with modules
+
+// const tutorial = require('./tutorial');
+// console.log(tutorial.sum(1,1));
+// console.log(tutorial.PI);
+// console.log(new tutorial.SomeMathObject());
+
+
+
+// 2.The Event Module and EventEmitter Class 
+
+const EventEmitter = require('events');
+const EventEMitter = new EventEmitter(); 
---
+++ tutorial.js
@@ -0,0 +1,14 @@
+// 1. Working with modules 
+
+// const sum = (num1, num2) => num1 + num2;
+// const PI = 3.14;
+// class SomeMathObject{
+//     constructor(){
+//         console.log("Object Created");
+//     }
+// }
+// module.exports = {sum : sum,PI : PI,SomeMathObject : SomeMathObject};
+
+
+
+// 2.The Event Module and EventEmitter Class 
