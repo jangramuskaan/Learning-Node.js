6c8d2cbc8e9037806f890b9446cf7badb4ac827c
Muskaan Jangra <jangra.muskaan.mj@gmail.com>
2026-06-17 11:51:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -207,16 +207,16 @@
 
 // 8.Creating HTTP Server and HTTP Module
 
-const http = require('http');
-const server = http.createServer((req,res) => {
-    if(req.url === '/'){
-        res.write('Hello World from Nodejs');
-        res.end();
-    }
-    else{
-        res.write('using some other domain');
-        res.end();
-    }
-});
+// const http = require('http');
+// const server = http.createServer((req,res) => {
+//     if(req.url === '/'){
+//         res.write('Hello World from Nodejs');
+//         res.end();
+//     }
+//     else{
+//         res.write('using some other domain');
+//         res.end();
+//     }
+// });
 
-server.listen('3000');
+// server.listen('3000');
