10c8d2cf31bb836c3546190937ec855c517b8e19
Muskaan Jangra <jangra.muskaan.mj@gmail.com>
2026-06-17 10:45:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -166,12 +166,12 @@
 
 // 5.Working with Readable and Writable Streams
 
-const fs = require('fs');
-const readStream = fs.createReadStream('./example.txt','utf-8');
-const writeStream = fs.createWriteStream('example2.txt');
-readStream.on('data', (chunk) => {
-    writeStream.write(chunk);
-});
+// const fs = require('fs');
+// const readStream = fs.createReadStream('./example.txt','utf-8');
+// const writeStream = fs.createWriteStream('example2.txt');
+// readStream.on('data', (chunk) => {
+//     writeStream.write(chunk);
+// });
 
 
 
